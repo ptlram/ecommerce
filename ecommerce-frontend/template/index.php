4033d669e517a36e7b7991b98edf3b7cfe04fc9d
ptlraj <rajvp1103@gmail.com>
2025-03-25 14:57:43 +0530

    product DOLAR TO RUPPES

--- ecommerce-frontend/template/index.php
+++ ecommerce-frontend/template/index.php
@@ -124,8 +124,8 @@ $ssubcategory = $query->fetchAll();
                                             <button type="button" class="btn btn-secondary btn-sm float-right">
                                                 <i class="mdi mdi-cart-outline"></i> Add To Cart
                                             </button>
-                                            <p class="offer-price mb-0">$<?= number_format($product['base_price'], 2) ?> <i class="mdi mdi-tag-outline"></i><br>
-                                                <span class="regular-price">$<?= number_format($product['mrp'], 2) ?></span>
+                                            <p class="offer-price mb-0">₹<?= number_format($product['base_price'], 2) ?> <i class="mdi mdi-tag-outline"></i><br>
+                                                <span class="regular-price">₹<?= number_format($product['mrp'], 2) ?></span>
                                             </p>
                                         </div>
                                     </a>
--- ecommerce-frontend/template/shop.php
+++ ecommerce-frontend/template/shop.php
@@ -116,7 +116,7 @@
             <span class="badge badge-success">50% OFF</span>
             <h5><a href="#">Product Title Here</a></h5>
             <h6><strong><span class="mdi mdi-approval"></span> Available in</strong> - 500 gm</h6>
-            <p class="offer-price mb-0">$450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">$800.99</span></p>
+            <p class="offer-price mb-0">₹450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">₹800.99</span></p>
          </div>
          <div class="cart-list-product">
             <a class="float-right remove-cart" href="#"><i class="mdi mdi-close"></i></a>
@@ -124,7 +124,7 @@
             <span class="badge badge-success">50% OFF</span>
             <h5><a href="#">Product Title Here</a></h5>
             <h6><strong><span class="mdi mdi-approval"></span> Available in</strong> - 500 gm</h6>
-            <p class="offer-price mb-0">$450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">$800.99</span></p>
+            <p class="offer-price mb-0">₹450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">₹800.99</span></p>
          </div>
          <div class="cart-list-product">
             <a class="float-right remove-cart" href="#"><i class="mdi mdi-close"></i></a>
@@ -132,7 +132,7 @@
             <span class="badge badge-success">50% OFF</span>
             <h5><a href="#">Product Title Here</a></h5>
             <h6><strong><span class="mdi mdi-approval"></span> Available in</strong> - 500 gm</h6>
-            <p class="offer-price mb-0">$450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">$800.99</span></p>
+            <p class="offer-price mb-0">₹450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">₹800.99</span></p>
          </div>
          <div class="cart-list-product">
             <a class="float-right remove-cart" href="#"><i class="mdi mdi-close"></i></a>
@@ -140,7 +140,7 @@
             <span class="badge badge-success">50% OFF</span>
             <h5><a href="#">Product Title Here</a></h5>
             <h6><strong><span class="mdi mdi-approval"></span> Available in</strong> - 500 gm</h6>
-            <p class="offer-price mb-0">$450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">$800.99</span></p>
+            <p class="offer-price mb-0">₹450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">₹800.99</span></p>
          </div>
          <div class="cart-list-product">
             <a class="float-right remove-cart" href="#"><i class="mdi mdi-close"></i></a>
@@ -148,16 +148,16 @@
             <span class="badge badge-success">50% OFF</span>
             <h5><a href="#">Product Title Here</a></h5>
             <h6><strong><span class="mdi mdi-approval"></span> Available in</strong> - 500 gm</h6>
-            <p class="offer-price mb-0">$450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">$800.99</span></p>
+            <p class="offer-price mb-0">₹450.99 <i class="mdi mdi-tag-outline"></i> <span class="regular-price">₹800.99</span></p>
          </div>
       </div>
       <div class="cart-sidebar-footer">
          <div class="cart-store-details">
-            <p>Sub Total <strong class="float-right">$900.69</strong></p>
-            <p>Delivery Charges <strong class="float-right text-danger">+ $29.69</strong></p>
-            <h6>Your total savings <strong class="float-right text-danger">$55 (42.31%)</strong></h6>
+            <p>Sub Total <strong class="float-right">₹900.69</strong></p>
+            <p>Delivery Charges <strong class="float-right text-danger">+ ₹29.69</strong></p>
+            <h6>Your total savings <strong class="float-right text-danger">₹55 (42.31%)</strong></h6>
          </div>
-         <a href="checkout.html"><button class="btn btn-secondary btn-lg btn-block text-left" type="button"><span class="float-left"><i class="mdi mdi-cart-outline"></i> Proceed to Checkout </span><span class="float-right"><strong>$1200.69</strong> <span class="mdi mdi-chevron-right"></span></span></button></a>
+         <a href="checkout.html"><button class="btn btn-secondary btn-lg btn-block text-left" type="button"><span class="float-left"><i class="mdi mdi-cart-outline"></i> Proceed to Checkout </span><span class="float-right"><strong>₹1200.69</strong> <span class="mdi mdi-chevron-right"></span></span></button></a>
       </div>
    </div>
    <!-- Bootstrap core JavaScript -->
